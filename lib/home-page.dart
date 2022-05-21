9d4406908aa20d266012f5b95d00b0de9db195e7
fidel <fidelesodoga@gmail.com>
2022-05-21 01:44:15 +0100

    second commit

---
+++ lib/home-page.dart
@@ -0,0 +1,68 @@
+
+import 'dart:convert';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _items = [];
+
+  // Fetch content from the json file
+  Future<void> readJson() async {
+    final String response = await rootBundle.loadString('assets/posts.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["items"];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'TP2: Json users reader',
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('Load Data'),
+              onPressed: readJson,
+            ),
+
+            // Display the data loaded from sample.json
+            _items.isNotEmpty
+                ? Expanded(
+              child: ListView.builder(
+                itemCount: _items.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      leading: Text("${_items[index]["id"]}"),
+                      title: Text("${_items[index]["title"]}",style: TextStyle(fontWeight: FontWeight.bold),),
+                      subtitle: Text("${_items[index]["body"]}"),
+                    ),
+                  );
+                },
+              ),
+            )
+                : Container()
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
+import 'package:tp2_cours_flutter/home-page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,73 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      // Hide the debug banner
-      debugShowCheckedModeBanner: false,
-      title: "Json users reader",
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List _items = [];
-
-  // Fetch content from the json file
-  Future<void> readJson() async {
-    final String response = await rootBundle.loadString('assets/posts.json');
-    final data = await json.decode(response);
-    setState(() {
-      _items = data["items"];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Json users reader',
+    return MaterialApp(
+      // Remove the debug banner
+        debugShowCheckedModeBanner: false,
+        title: 'Tp2 app',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
         ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            ElevatedButton(
-              child: const Text('Load Data'),
-              onPressed: readJson,
-            ),
-
-            // Display the data loaded from sample.json
-            _items.isNotEmpty
-                ? Expanded(
-              child: ListView.builder(
-                itemCount: _items.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    margin: const EdgeInsets.all(10),
-                    child: ListTile(
-                      leading: Text("${_items[index]["id"]}"),
-                      title: Text(_items[index]["title"]),
-                      subtitle: Text(_items[index]["body"]),
-                    ),
-                  );
-                },
-              ),
-            )
-                : Container()
-          ],
-        ),
-      ),
-    );
+        home:  HomePage());
   }
 }
